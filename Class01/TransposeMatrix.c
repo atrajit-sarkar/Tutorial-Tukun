c13de5ebd3524007722b055eb513b5407bc19e89
atrajit-sarkar <atrajit.sarkar@gmail.com>
2024-11-06 09:26:39 +0530

    updTED

--- Class01/TransposeMatrix.c
+++ Class01/TransposeMatrix.c
@@ -3,7 +3,7 @@
 int main(){
     // A[i][j]-----> A[j][i]
 
-    int m = 2, n = 2;
+    int m = 2, n = 3;
     float A[m][n];
     float B[m][n];
 
@@ -33,26 +33,26 @@ int main(){
     // Displaying The matrix Transpose of A ...
     // Switch in only one place [either switch m and n or i and j ]
     
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            printf("%f\t", A[j][i]);
-            // scanf("%f",&A[i][j]);
-        }
-        printf("\n");
-    }
-
-
-    // printf("_____________________________________\n");
-    // for (int j = 0; j < n; j++)
+    // for (int i = 0; i < m; i++)
     // {
-    //     for (int i = 0; i < m; i++)
+    //     for (int j = 0; j < n; j++)
     //     {
-    //         printf("%f\t", A[i][j]);
+    //         printf("%f\t", A[j][i]);
     //         // scanf("%f",&A[i][j]);
     //     }
     //     printf("\n");
     // }
+
+
+    // printf("_____________________________________\n");
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            printf("%f\t", A[i][j]);
+            // scanf("%f",&A[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
