5a0aae2a6512b8072d924660287189b715171dc1
atrajit-sarkar <atrajit.sarkar@gmail.com>
2024-11-29 15:07:32 +0530

    updaete

--- Class05/cleanUnion.c
+++ Class05/cleanUnion.c
@@ -14,19 +14,13 @@ int main()
     printf("%d\n", x.a);
 
     x.b = 11.2;
-    printf("%d\n", x.a);
     printf("%f\n", x.b);
 
     x.array[0] = 1;
     printf("%d\n", x.array[0]);
-    x.array[1] = 2;
+
     x.array[1] = 5;
     printf("%d\n", x.array[1]);
-    printf("%d\n", x.array[0]);
-
-    // int a; // Initialise.........
-
-    // printf("%d", );
 
     return 0;
 }
